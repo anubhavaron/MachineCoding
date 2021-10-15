2e0380f1e6ae0132815a8bedc2e0faef4ef0b3fa
Anubhav Aron <anubhav.aron@BLRETV-C02XP0C.local>
2021-10-16 04:27:17 +0530

    Removed Old library code

--- library/src/main/java/com/coding/library/DriverClass.java
+++
@@ -1,4 +0,0 @@
-package com.coding.library;
-
-public class DriverClass {
-}
--- library/src/main/java/com/coding/library/constants/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.coding.library.constants;
-
-public class Constants {
-
-    public static final Integer DEFAULT_BORROW_LIMIT = 5;
-}
